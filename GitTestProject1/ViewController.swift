3530b507e233fd35299fa9a3f530c6b9bd84ccf6
Yoshiki Chinen <pika13407223@gmail.com>
2020-06-04 15:48:37 +0900

    print(あいうえお！！）を追記しました Chinen Yoshiki

---
+++ GitTestProject1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject1
+//
+//  Created by Yoshiki Chinen on 2020/06/03.
+//  Copyright © 2020 Yoshiki Chinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     print("あいうえお！！！！")
+        
+        
+    }
+
+
+}
+
